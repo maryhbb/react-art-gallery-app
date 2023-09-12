2bf3bca4be93475a016184eae6187c08eb5a27ac
Mary <76174247+maryhbb@users.noreply.github.com>
2023-09-12 16:48:37 +0200

    add SWR global configuration

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
   return (
     <div>
       <h1>Hello from Next.js</h1>
